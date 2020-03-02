641a48ab19b7b1ebf43e6d52ffb0b8ebbc44ed79
Vinicius Amorim <amorimhavoc@gmail.com>
2020-03-02 13:55:00 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/curso/entities/User.java
+++ src/main/java/com/example/curso/entities/User.java
@@ -1,4 +1,89 @@
-package com.example.curso;
+package com.example.curso.entities;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.swing.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(long id, String name, String email, String phone, String password) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/example/curso/resource/UserResource.java
+++ src/main/java/com/example/curso/resource/UserResource.java
@@ -1,4 +1,18 @@
-package com.example.curso;
+package com.example.curso.resource;
 
+import com.example.curso.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> finAll(){
+        User u = new User(1L,"maria", "maria@gmailcom","9999999999","12345");
+        return ResponseEntity.ok().body(u);
+    }
 }
