8f400b0310d1b043f8490a63030bd98f6aa99104
Vinicius <amorimhavoc@gmail.com>
2020-03-05 16:17:03 -0300

    Subtotal & Total methods

--- src/main/java/com/example/curso/entities/OrderItem.java
+++ src/main/java/com/example/curso/entities/OrderItem.java
@@ -65,6 +65,10 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    public double getSubTotal(){
+        return price* quantity;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
